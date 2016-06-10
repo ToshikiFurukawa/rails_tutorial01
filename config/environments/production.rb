ffab519e4a1316c7ea22bd3469aa3d72faa69025
Toshiki Furukawa <furu.toshiki@gmail.com>
2016-06-10 17:32:19 +0900

    fix productin.rb host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '【hidden-island-90811】.herokuapp.com'
+  host = 'hidden-island-90811.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
